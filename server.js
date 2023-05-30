1cb0fee7e341e3abd27bb27fcd22b4651a0bea8e
kimmh891223 <kimmh891223@hotmail.com>
2023-05-29 22:14:48 -0400

    fixed bugs

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const fs = require("fs");
 const { v4: uuidv4 } = require("uuid");
-const notesData = require('/db/db.json');
+const notesData = require('./db/db.json');
 const PORT = process.env.PORT || 3001;
 const path = require("path");
 
@@ -12,11 +12,11 @@ app.use(express.json());
 app.use(express.urlencoded({extended: true}));
 
 app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, '/public/index.html'));
+    res.sendFile(path.join(__dirname, './public/index.html'));
   });
 
 app.get('/notes', (req, res) => {
-    res.sendFile(path.join(__dirname, '/public/notes.html'));
+    res.sendFile(path.join(__dirname, './public/notes.html'));
   });
 
 app.get('/api/notes', (req, res) => res.json(notesData));
@@ -24,7 +24,7 @@ app.get('/api/notes', (req, res) => res.json(notesData));
 app.post('/api/notes', (req, res) => {
   req.body.id= uuidv4();
   notesData.push(req.body)
-  fs.writeFileSync('/db/db.json',JSON.stringify(notesData));
+  fs.writeFileSync('./db/db.json',JSON.stringify(notesData));
   console.info(`${req.method} request received to add a note`);
   res.json(notesData);
   console.log(uuidv4());
@@ -37,7 +37,7 @@ app.delete('/api/notes/:id', (req, res) => {
     }
     
   }
-  fs.writeFileSync('/db/db.json',JSON.stringify(notesData));
+  fs.writeFileSync('./db/db.json',JSON.stringify(notesData));
   console.info(`${req.method} request received to add a note`);
   res.json(notesData);
   })
